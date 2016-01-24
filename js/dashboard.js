fbbb29ec38405f5dff64f7f5a68aca7cec482a29
Henorvell <comword@live.com>
2016-01-24 18:30:56 +0800

    add some images and prepare for templetes

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,3 +3,17 @@ $(document).ready(function () {
     $('.row-offcanvas').toggleClass('active')
   });
 });
+function getPage(tag) {
+  $.ajax({
+    url: "templetes/"+tag,
+    type: 'GET',
+    timeout: 30000,
+    error: function(){
+      return true;
+    },
+    success: function(content){
+      $('[id="Mycanvas"]').empty();
+      $('[id="Mycanvas"]').append(content);
+    }
+  });
+}
